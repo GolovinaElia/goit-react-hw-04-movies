d1b92358a99a5d96038c81fcf9309e71bfecb210
GolovinaElia <eliagolovina@ukr.net>
2021-05-19 17:58:52 +0300

    Добавляет файлы

--- src/views/MovieDetailsPage.js
+++ src/views/MovieDetailsPage.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { NavLink } from 'react-router-dom';
 
 const BASE_URL = 'https://api.themoviedb.org';
 const KEY_URL = 'be8c1fddab60d3ca36450ce7d48f58dd';
 class MovieDetailsPage extends Component {
   state = {
-    movies: [],
+    id: null,
+    poster_path: null,
+    original_title: null,
+    release_date: null,
+    overview: null,
+    genres: null,
   };
 
   async componentDidMount() {
+    const { movieId } = this.props.match.params;
     const response = await axios.get(
-      `${BASE_URL}/3/movie/popular?api_key=${KEY_URL}&language=en-US&page=1`,
+      `${BASE_URL}/3/movie/${movieId}?api_key=${KEY_URL}&language=en-US`,
     );
-    // console.log(response.data.results);
-    this.setState({ movies: response.data.results });
+    this.setState({ ...response.data });
   }
   render() {
+    const { original_title, poster_path, release_date, overview, id } =
+      this.state;
     return (
       <>
-        <h2>Это страница с детальной информацией о фильме</h2>;
+        <div>
+          <img src={poster_path} alt="" />
+          <h2>{original_title}</h2>
+          <p>{release_date}</p>
+          <p>Overview {overview}</p>
+        </div>
+        <div>
+          <p>
+            <NavLink to={`${this.props.match.url}/${id}/cast`}>Cast</NavLink>
+          </p>
+          <p>
+            <NavLink to="/movies/:movieId/reviews">Reviews</NavLink>
+          </p>
+        </div>
       </>
     );
   }
